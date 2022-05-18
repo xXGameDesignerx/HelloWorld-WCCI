8f7fd6df810c004951050ae7a35e702fb205cbec
Xavier M <golf4Xavier@gmail.com>
2022-05-18 13:58:26 -0400

    updating github folder

---
+++ HelloWorld-WCCI/HelloWorld-WCCI/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Variable explanation
+// What is a variable?
+// named object/values that can change
+//variablke are named objects with values and types
+//built-in types: string, int, double, boolean
+// String = text, int = whole numbers, double = decimals, boolean = true/false
+
+
+// variable declaration is creating a variable
+// variableType variableName;
+string instructor; // this variable is null (no value)
+
+//variable assignment is giving a variable a value
+// variableName = value;
+instructor = "davis"; // instructor has a value of davis (setting)
+instructor = "gavin";
+
+instructor = "gavin" + "davis";
+
+Console.WriteLine(instructor); // instructor has a vaule of gavin
+
+string coInstructor = "gavin";
+int studentCount = 8;
+
+// declaring a variable and assigning variable
+string input = Console.ReadLine();
+Console.WriteLine(input);
